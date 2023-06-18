978fef25119c2fc7a01d4cc92c4369e9ea98a65f
Levin Løssfelt <levin.lossfelt@gmail.com>
2023-06-18 22:18:32 +0200

    Første push til repository

---
+++ src/Board.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Board.css'
+
+export function TicTacToeBoard({ ctx, G, moves }) {
+  const onClick = (id) => moves.clickCell(id);
+
+  let winner = '';
+  if (ctx.gameover) {
+    winner =
+      ctx.gameover.winner !== undefined ? (
+        <div id="winner">Winner: {ctx.gameover.winner === "0" ? ("☢") : ("☣")}</div>
+      ) : (
+        <div id="winner">Draw!</div>
+      );
+  }
+
+  let tbody = [];
+  for (let i = 0; i < 3; i++) {
+    let cells = [];
+    for (let j = 0; j < 3; j++) {
+      const id = 3 * i + j;
+      cells.push(
+        <td key={id}>
+          {G.cells[id] ? (
+            <button className='knapp'>{G.cells[id] === "0" ? ("☢") : ("☣")}</button>
+          ) : (
+            <button className='knapp' onClick={() => onClick(id)} />
+          )}
+        </td>
+      );
+    }
+    tbody.push(<tr key={i}>{cells}</tr>);
+  }
+
+  return (
+    <div className="container">
+      <div className='center-content'>
+        <table>
+          <tbody>{tbody}</tbody>
+        </table>
+        <div className='text'>
+          {winner}
+        </div>
+      </div>
+    </div>
+  );
+}
