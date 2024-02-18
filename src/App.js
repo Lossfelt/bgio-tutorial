8502c1c62a7b7b5a39a0476f1e46f7f5dd8c982d
Levin Løssfelt <levin.lossfelt@gmail.com>
2024-02-18 23:18:05 +0100

    Lobby works! CORS was the solution.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import { Client } from 'boardgame.io/react';
 import { TicTacToe } from './Game';
 import { TicTacToeBoard } from './Board';
-import { SocketIO } from 'boardgame.io/multiplayer';
-import React, { useState } from 'react';
+import React from 'react';
 import './Board.css';
 import { Lobby } from 'boardgame.io/react';
 
-const TicTacToeClient = Client({
-    game: TicTacToe,
-    board: TicTacToeBoard,
-    multiplayer: SocketIO({ server: '192.168.4.24:8000' }),
-});
-
 const App = () => {
-
-    const [playerchoice, setPlayerchoice] = useState(null);
-    const handlePlayerchoice = (key) => {
-        setPlayerchoice(key);
-    };
 
     return (
         <div className='center-content'>
-            {/* {playerchoice ? (<TicTacToeClient playerID={playerchoice} />) :
-                (<div><button onClick={() => handlePlayerchoice('0')}>Player 0</button> <button onClick={() => handlePlayerchoice('1')}>Player 1</button></div>)} */}
             <Lobby
                 gameServer={`http://${window.location.hostname}:8000`}
-                lobbyServer={`http://${window.location.hostname}:8000`}/* Previous adress: https://192.168.4.24:8000 */
+                lobbyServer={`http://${window.location.hostname}:8000`}
                 gameComponents={[
                     { game: TicTacToe, board: TicTacToeBoard, }
                 ]}
--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const {Server, Origins} = require('boardgame.io/server');
 const {TicTacToe} = require('./Game');
+const cors = require('@koa/cors');
 
 const server = Server({
     games: [TicTacToe],
-    origins: [Origins.LOCALHOST, Origins.LOCALHOST_IN_DEVELOPMENT],
 });
 
-//server.run(8000);
+server.app.middleware.unshift(cors());
+
 server.run(8000,()=>{console.log("server is running...")});
