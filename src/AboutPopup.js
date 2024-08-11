e0822e127345f7163019a1180babbac3c6f8c84a
Levin Løssfelt <levin.lossfelt@gmail.com>
2024-08-11 23:00:59 +0200

    Lagt inn bakgrunn i popup, samt endret formatet noe.

--- src/AboutPopup.js
+++ src/AboutPopup.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Board.css';
 
 const AboutPopup = ({ onClose }) => {
   return (
     <div className="popup">
-      <div className="popup-content">
+      <div>
         <button onClick={onClose} className="close-button">Close</button>
         <h2>Background</h2>
         <p>
@@ -12,14 +13,26 @@ const AboutPopup = ({ onClose }) => {
         The now powerful Mexican queendom and the Pan-Canadia Inuit Alliance are both greedily claiming territory. Clashes are unavoidable.
         </p>
         <h2>The Mexican Queendom</h2>
+        <img style={{ width: '50%' }} src={process.env.PUBLIC_URL + '/Mexican_Queendom.jpg'} alt="Mexican Queendom flag" />
         <p>
-        The Mexican Queendom is a matriarchal society where the queen is both the head of state and the head of the church.
-        The Queendom is a mix of Aztec and Spanish culture, with a strong emphasis on the Aztec warrior culture.
+        The Mexican Queendom is a matriarchal society where the queen is both the head of state, the head of the church and the head of the cartels.
+        It's a totalitarian regime where the queen's word is law, controlling every level of society.
+        Information is tightly controlled and dissent is not tolerated. The Mexican Queendom is a mix of traditional Mexican culture combined with a glorified roman empire.
+        Their land based military is second to none.
         </p>
         <h2>The Pan-Canadia Inuit Alliance</h2>
+        <img style={{ width: '50%' }} src={process.env.PUBLIC_URL + '/Pan-Canadia.jpg'} alt="Pan-Canadia Inuit Alliance flag" />
         <p>
-        The Pan-Canadia Inuit Alliance is a confederation of Inuit tribes that have banded together to protect their lands.
-        The Alliance is a mix of traditional Inuit culture and modern Canadian culture, with a strong emphasis on Inuit hunting traditions.
+        The Pan-Canadia Inuit Alliance is a confederation of Inuit tribes that originally banded together to protect their lands.
+        The Alliance has through deft political manouvering and cunning trade deals managed to become a major player on the world stage.
+        On the surface it's a democracy, but in reality the Inuit-led businesses and corporations control the government.
+        The Pan-Canadia Inuit Alliance is a mix of traditional Inuit culture combined with a modern capitalist society.
+        Their navy is the strongest in the world.
+        </p>
+        <h2>Rules</h2>
+        <p>
+        In this game, you will play as either the Mexican Queendom or the Pan-Canadia Inuit Alliance.
+        Your goal is to be the first to have three of your faction's flag in a row, either horizontally, vertically, or diagonally.
         </p>
       </div>
     </div>
